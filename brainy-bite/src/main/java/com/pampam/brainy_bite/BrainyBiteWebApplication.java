9d81f347537de69976f775d186a4225452e0b582
Thara Sritharadol <sunsummerone1@gmail.com>
2024-11-20 13:32:22 +0700

    start

---
+++ brainy-bite/src/main/java/com/pampam/brainy_bite/BrainyBiteWebApplication.java
@@ -0,0 +1,13 @@
+package com.pampam.brainy_bite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainyBiteWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrainyBiteWebApplication.class, args);
+	}
+
+}
