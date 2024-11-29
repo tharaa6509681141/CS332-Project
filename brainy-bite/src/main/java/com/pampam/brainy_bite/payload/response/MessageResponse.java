d4a2c697f5bc72a2a7f8ef4b02ab34742a9b491a
Thara Sritharadol <sunsummerone1@gmail.com>
2024-11-29 14:11:35 +0700

    Create Payload

---
+++ brainy-bite/src/main/java/com/pampam/brainy_bite/payload/response/MessageResponse.java
@@ -0,0 +1,17 @@
+package com.pampam.brainy_bite.payload.response;
+
+public class MessageResponse {
+    private String message;
+
+    public MessageResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
