e11cdbf99e2c62ccc16ad2990157df714a45c5bd
Thara Sritharadol <sunsummerone1@gmail.com>
2024-12-03 20:42:14 +0700

    Noyy 2

---
+++ brainy-bite/src/main/resources/static/script/navbar.js
@@ -0,0 +1,7 @@
+function openForm() {
+    document.getElementById("myOverlay").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myOverlay").style.display = "none";
+}
