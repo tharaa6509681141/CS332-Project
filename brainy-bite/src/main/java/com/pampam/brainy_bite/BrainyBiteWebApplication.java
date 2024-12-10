194d4de88a849cc46302407a46546871d88b3dd5
Thara Sritharadol <sunsummerone1@gmail.com>
2024-12-10 14:43:41 +0700

    Create AWS S3 Service
    Create S3 Configuration, Service, Controller, and Hide AWS Key in application.properties

--- brainy-bite/src/main/java/com/pampam/brainy_bite/BrainyBiteWebApplication.java
+++ brainy-bite/src/main/java/com/pampam/brainy_bite/BrainyBiteWebApplication.java
@@ -1,13 +1,33 @@
 package com.pampam.brainy_bite;
 
+import com.amazonaws.services.s3.model.AmazonS3Exception;
+import com.pampam.brainy_bite.service.S3Service;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@Log4j2
 public class BrainyBiteWebApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BrainyBiteWebApplication.class, args);
+	}
+
+	@Bean
+	public ApplicationRunner applicationRunner(S3Service s3Service) {
+		return args -> {
+			log.info("Spring Boot AWS S3 integration...");
+
+			/*try {
+				var s3Object = s3Service.getFile("jvm.png");
+				log.info(s3Object.getKey());
+			} catch (AmazonS3Exception e) {
+				log.error(e.getMessage());
+			}*/
+		};
 	}
 
 }
